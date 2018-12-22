5a485da04ea950842842349564757cc6cadb1f48
Asaf David <asafd1@gmail.com>
2018-12-23 00:33:05 +0200

    send message

---
+++ bot.js
@@ -0,0 +1,17 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '761179774:AAESPJY8w9EjDXfBAnPrzVE_obv8RULBygI';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: false});
+
+const opts = {
+    reply_markup: JSON.stringify({
+      inline_keyboard: [
+        [{"text":"כן", "callback_data":"yes"},{"text":"אולי", "callback_data":"maybe"},{"text":"לא", "callback_data":"no"}]
+      ],
+      "resize_keyboard" : true,
+    })
+  };
+bot.sendMessage("-1001428218098", "מגיע?", opts);
