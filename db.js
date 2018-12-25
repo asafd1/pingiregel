cca8ef82a58144ef4b6e029fda56130638c13c7c
Asaf David <asafd1@gmail.com>
2018-12-26 00:24:24 +0200

    add some CRUD opertaions
     CRUD opertaions with some endpoints to invoke them

--- db.js
+++ db.js
@@ -13,7 +13,60 @@ MongoClient.connect(mongouri, function(err, mongodb) {
     }
 );
 
-exports.getSetting = function (_key) {
-    console.log("getting " + _key);
-    return db.collection("settings").findOne({key:_key});
+// SETTINGS
+exports.getSetting = function (key) {
+    console.log("getting setting: " + key);
+    return db.collection("settings").findOne({key:key});
+}
+
+exports.deleteSetting = function (key) {
+    console.log("delete setting: " + key);
+    return db.collection("settings").deleteOne({key:key});
+}
+
+exports.insertSetting = function (setting) {
+    console.log("insert setting: " + setting);
+    return db.collection("settings").insertOne(setting);
+}
+
+exports.getSettings = function () {
+    console.log("getting settgins");
+    return db.collection("settings").find({}).toArray();
+}
+
+// MISC
+exports.getMisc = function (key) {
+    console.log("getting misc: " + key);
+    return db.collection("misc").findOne({key:key});
+}
+
+exports.deleteMisc = function (key) {
+    console.log("delete misc: " + key);
+    return db.collection("misc").deleteOne({key:key});
+}
+
+exports.insertMisc = function (setting) {
+    console.log("insert misc: " + setting);
+    return db.collection("misc").insertOne(setting);
+}
+
+// PLAYERS
+exports.getPlayer = function (id) {
+    console.log("getting player by id: " + id);
+    return db.collection("players").findOne({id:id});
+}
+
+exports.getPlayers = function () {
+    console.log("getting all players");
+    return db.collection("players").find({}).toArray();
+}
+
+exports.insertPlayer = function (player) {
+    console.log("adding player: " + player);
+    return db.collection("players").insertOne(player);
+}
+
+exports.updatePlayer = function (id, player) {
+    console.log("updating player by id: " + id);
+    return db.collection("players").updateOne({id:id}, { $set: player });
 }
--- server.js
+++ server.js
@@ -1,26 +1,55 @@
 var express = require('express');
 var app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
 var DB = require("./db");
 const port = 80;
 
-app.get('/', function(_request, response) {
-  p = DB.getSetting("targetForGame");
-  p.then((p) => {response.send(p)});
-  });
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-// const http = require('http');
+function sendResponse(response, value) {
+  if (value) {
+    response.send(value)
+  } else {
+    response.send(404);
+  }
+}
 
-// const hostname = '0.0.0.0';
-// const port = 80;
+app.route('/settings')
+.get(function(_request, response, next) {
+  p = DB.getSettings();
+  p.then((value) => {sendResponse(response, value)});
+})
+.post(function(request, response, next) {
+  p = DB.insertSetting(request.body);
+  p.then((value) => {sendResponse(response, value)});
+});
 
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('pingiregel\n');
-// });
+app.route('/settings/:setting')
+.get(function(request, response, next) {
+  p = DB.getSetting(request.params.setting);
+  p.then((value) => {sendResponse(response, value)});
+})
+.delete(function(request, response, next) {
+  p = DB.deleteSetting(request.params.setting);
+  p.then((value) => {sendResponse(response, value)});
+});
 
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
+app.route('/misc/:setting')
+.get(function(request, response, next) {
+  p = DB.getMisc(request.params.setting);
+  p.then((value) => {sendResponse(response, value)});
+})
+.delete(function(request, response, next) {
+  p = DB.deleteMisc(request.params.setting);
+  p.then((value) => {sendResponse(response, value)});
+});
+
+app.route('/misc')
+.post(function(request, response, next) {
+  p = DB.insertMisc(request.body);
+  p.then((value) => {sendResponse(response, value)});
+});
