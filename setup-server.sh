df0b1756c80b59afe62a9144d1769f0d643082e1
Asaf David <asafd1@gmail.com>
2019-01-02 13:02:10 +0000

    fix security breach

--- setup-server.sh
+++ setup-server.sh
@@ -1,8 +1,10 @@
+# 1. install node
+# 2. install mongo
+# 3. generate key pair (see script)
+# 4. init db (see script)
 curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
 sudo apt-get -y install nodejs
 
 sudo apt install -y mongodb
 sudo systemctl status mongodb
-
-
 
