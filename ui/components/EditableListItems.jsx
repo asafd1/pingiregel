4f2e9c56edd5534001bb2b2641719fe2069ac02b
Asaf David <adavid@pingidentity.com>
2019-12-16 08:39:31 +0200

    EditableUser.jsx

--- ui/components/EditableListItems.jsx
+++ ui/components/EditableListItems.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import FilteredList from './FilteredList.jsx';
+import EditableUser from './EditableUser.jsx';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 class EditableListItems extends React.Component {
-    constructor(props) {
+    constructor(props) {    
         super(props);
         this.state = {
             items: this.props.items,
@@ -15,35 +16,61 @@ class EditableListItems extends React.Component {
     }
 
     handleItemSelected(i) {
-        this.setState({ selected : i });
+        this.setState({ 
+            selected : i,
+            dirty : false
+        });
         console.log(`${this.props.items[i]} was selected`);
     }
 
     handleItemDeselected(i) {
+        this.setState({ 
+            selected : -1,
+            dirty : false
+        });
         console.log(`${this.props.items[i]} was unselected`);
     }
 
+    setDirty(dirty) {
+        this.setState({ 
+            dirty : dirty
+        });
+    }
+
+    renderUser() {
+        const user = {
+            username: this.state.items[this.state.selected],
+            firstname: "fn " + this.state.items[this.state.selected],
+            lastname: "ln " + this.state.items[this.state.selected]
+        }
+        if (this.state.selected >= 0) {
+            return (
+                <Col md="6" style={{ marginTop: '59px', backgroundColor: 'snow', height: '309px' }}>
+                    <EditableUser 
+                        user={user} 
+                        dirty={this.state.dirty} 
+                        setDirty={this.setDirty.bind(this)}></EditableUser>
+                </Col>
+            )
+        }
+    }
+    
     render() {
         return (
             <Container>
-            <Row>
-                <Col id="heading">heading</Col>
-            </Row>
-            <Row>
-                <Col>
-                    <FilteredList 
-                        items={this.state.items} 
-                        handleItemSelected={(i) => this.handleItemSelected(i)}
-                        handleItemDeselected={(i) => this.handleItemDeselected(i)}
-                    />
-                </Col>
-                <Col>
-                    <Row style={{ marginTop: '59px', backgroundColor: 'lightgray', height: '100px' }}>{this.state.items[this.state.selected]}</Row>
-                </Col>
-            </Row>
-            <Row>
-                <Col>buttons</Col>
-            </Row>
+                <Row>
+                    <Col id="heading">heading</Col>
+                </Row>
+                <Row>
+                    <Col md="6">
+                        <FilteredList 
+                            items={this.state.items} 
+                            handleItemSelected={(i) => this.handleItemSelected(i)}
+                            handleItemDeselected={(i) => this.handleItemDeselected(i)}
+                        />
+                    </Col>
+                    {this.renderUser()}
+                </Row>
             </Container>
         );
     }
---
+++ ui/components/EditableUser.jsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import Form from 'react-bootstrap/Form';
+import FormGroup from 'react-bootstrap/FormGroup';
+import FormLabel from 'react-bootstrap/FormLabel';
+import FormControl from 'react-bootstrap/FormControl';
+
+class EditableUser extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    save() {
+        console.log("SAVE");
+        this.props.setDirty(false);
+    }
+
+    cancel() {
+        console.log("CANCEL");
+        this.props.setDirty(false);
+    }
+
+    renderButtons() {
+        if (this.props.dirty == false) {
+            return;
+        }
+        return (
+            <Row style={{bottom: 0, position: "absolute"}}>
+                <div>
+                    <Button 
+                        variant="outline-primary" 
+                        onClick={this.save.bind(this)}>
+                        Save
+                    </Button>
+                    {" "}
+                    <Button 
+                        variant="outline-secondary"
+                        onClick={this.cancel.bind(this)}>
+                        Cancel
+                    </Button>
+                </div>
+            </Row>
+        );
+    }
+
+    onClickUsername() {
+        this.props.setDirty(true);
+    }
+
+    render() {
+        return (
+
+            <Form>
+                <FormGroup as={Row} controlId="formPlaintextUsername" onClick={this.onClickUsername.bind(this)}>
+                    <div>
+                        <FormLabel column sm="20">
+                        Username:
+                        </FormLabel>
+                        {' '}
+                        <FormLabel column sm="20">{this.props.user.username}</FormLabel>
+                    </div>
+                </FormGroup>
+
+                <FormGroup as={Row} controlId="formPlaintextUsername">
+                    <div>
+                        <FormLabel column sm="20">
+                        First name:
+                        </FormLabel>
+                        {' '}
+                        <FormLabel column sm="20">{this.props.user.firstname}</FormLabel>
+                    </div>
+                </FormGroup>
+
+                <FormGroup as={Row} controlId="formPlaintextUsername">
+                    <div>
+                        <FormLabel column sm="20">
+                        Last name:
+                        </FormLabel>
+                        {' '}
+                        <FormLabel column sm="20">{this.props.user.lastname}</FormLabel>
+                    </div>
+                </FormGroup>
+                {
+                this.renderButtons()
+                }
+            </Form>
+
+            // <Container>
+                
+            //     <Row><Col id="username"  onClick={this.onClickUsername.bind(this)}>username:  {this.props.user.username}</Col></Row>
+            //     <Row><Col id="firstname" onClick={this.onClickUsername.bind(this)}>firstname: {this.props.user.firstname}</Col></Row>
+            //     <Row><Col id="lastname"  onClick={this.onClickUsername.bind(this)}>lastname:  {this.props.user.lastname}</Col></Row>
+            // </Container>
+        );
+    }
+}
+
+export default EditableUser;
