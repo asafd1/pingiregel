293902c5efc5db59a7c1a21e84749edd43cc1be9
Asaf David <adavid@pingidentity.com>
2019-12-16 08:41:04 +0200

    remove uncompleted code

--- ui/App.jsx
+++ ui/App.jsx
@@ -4,27 +4,17 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import EditableListItems from './components/EditableListItems.jsx';
 
-// var items = [
-//   'apple',
-//   'computer',
-//   'windows',
-//   'mouse',
-//   'keyboard',
-//   'monitor',
-//   'smartphone'
-// ];
+var items = [
+  'apple',
+  'computer',
+  'windows',
+  'mouse',
+  'keyboard',
+  'monitor',
+  'smartphone'
+];
 
-var DB = require("../src/db");
-DB.connect().then((db) => start(db)).catch((error) => errorHandler(error));
-
-function start(db) {
-  db.getPlayers().then((players) => {
-    items = _.map(players, (player) => {
-        return player.firstname;
-    })
-  });
-  ReactDOM.render(
-      <EditableListItems items={items}/>, 
-      document.getElementById('root')
-    );
-}
+ReactDOM.render(
+    <EditableListItems items={items}/>, 
+    document.getElementById('root')
+  );
